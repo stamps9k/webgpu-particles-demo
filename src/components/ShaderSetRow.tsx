fb097cdfbeed6465ef610e65d306a3c90d0520ee
Stampatron <james@nitori.local>
2026-05-11 14:10:29 +0900

    Initial implementation of dynamic shader configuration form.

--- src/components/ShaderSetRow.tsx
+++ src/components/ShaderSetRow.tsx
@@ -1,10 +1,19 @@
 import { response } from "express";
 import { useContext, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const ShaderSetRow = () => {
+	//Configuration values in query string
+	const [searchParams, set_search_params] = useSearchParams();
+
 	const [shader_sets, set_shader_sets] = useState([{ 
 		shader_set_id: 1, shader_set_name: "scatter-fade", shader_set_display_name: "Scatter Fade"
 	}]); 
+
+	const handle_shader_change = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		var shader_name = e.target.value;
+  	set_search_params({ "shader-set": shader_name });
+	};
 
 	//Get initial values for form
 	useEffect( () => {
@@ -22,7 +31,7 @@ const ShaderSetRow = () => {
 				<label htmlFor="shader">Shader: </label>
 			</div>
 			<div id="shaderElement" className="col-1">
-				<select id="shader-set" name="shader-set"> 
+				<select id="shader-set" name="shader-set" onChange={handle_shader_change}> 
 					{ shader_sets.map(
 						(shader_set) => (
 							<option key={shader_set.shader_set_id} value={shader_set.shader_set_name}>
--- src/components/ShadersConfigRows.tsx
+++ src/components/ShadersConfigRows.tsx
@@ -3,30 +3,48 @@ import { useEffect, useState } from "react";
 
 const ShaderConfigRows = () => {
 	//State variables
-	const [shader_configs, set_shader_configs] = useState<Array<Record<string,string>>>([{}]);
+	const [all_shader_configs, set_all_shader_configs] = useState<Record<string, Array<Record<string,string>>>>({});
+	const [shader_configs, set_shader_configs] = useState<Array<Record<string, string>>>([{}]);
 	const [options_map, set_options_map] = useState<Record<string, Array<string>>>({});
 
 	//Configuration values in query string
-	const [searchParams] = useSearchParams();
+	const [search_params, set_search_params] = useSearchParams();
 
 	//Get list of configuration items
 	useEffect( () => {
+		var shader_set_name = "scatter-fade"
+		if (search_params.get("shader-set") !== null && search_params.get("shader-set") !== undefined) {
+			shader_set_name = search_params.get("shader-set") as string;	
+		}
+
 		const fetchAll = async () => {
-			const results = await fetchConfigItems("1");
-			set_shader_configs(results);
+			const results = await fetchConfigItems();
+			const cached_configurations = parse_configs(results);
+			set_all_shader_configs(cached_configurations);
+			if (cached_configurations[shader_set_name] !== undefined) {
+				set_shader_configs(cached_configurations[shader_set_name]);
+			} else {
+				set_shader_configs([]);
+			}
 		}
 		fetchAll();
-	}, []);
+	}, [search_params]);
 
 	//Get initial values for configuration values
 	useEffect( () => {
+		var shader_set_name = "scatter-fade"
+		if (search_params.get("shader-set") !== null && search_params.get("shader-set") !== undefined) {
+			shader_set_name = "";	
+		}
+		
 		const fetchAll = async () => {
 			const results: Record<string, Array<string>> = {};
-			for (const shader_config of shader_configs) {
-				switch (shader_config.config_type_name) {
+			for (var shader_config of shader_configs) {
+			//for (const [key, value] of Object.entries(shader_configs[shader_set_name])) { 
+				switch (shader_config["config_type_name"]) {
 					case "SELECTION":
-						const options = await fetchOptions(shader_config.shader_config_id.toString());
-						const shader_config_id = shader_config.shader_config_id.toString();
+						const options = await fetchOptions(shader_config["shader_config_id"]);
+						const shader_config_id = shader_config["shader_config_id"].toString();
 						var allOptions: Array<string> = [] ;
 						for (const option of options) {
 							allOptions.push(option["config_value"]);
@@ -41,10 +59,10 @@ const ShaderConfigRows = () => {
 		fetchAll();
 	}, [shader_configs]);
 
-	async function fetchConfigItems(shader_id: string): Promise<Array<Record<string, string>>> {
+	async function fetchConfigItems(): Promise<Array<Record<string, string>>> {
 		try {
-			const shader_set = searchParams.get("shader-set");
-			const result = await fetch("/api/shader-configs?shader_set=" + shader_set);
+			const shader_set = search_params.get("shader-set");
+			const result = await fetch("/api/shader-configs");
 			const data = await result.json();
 			return data.message;
 		} catch (e) {
@@ -64,6 +82,22 @@ const ShaderConfigRows = () => {
 			};
 	};
 
+	function parse_configs(configs: Array<Record<string, string>>): Record<string, Array<Record<string, string>>> {
+		var results: Record<string, Array<Record<string, string>>> = {};
+
+		for (const shader_config of configs) {
+			const shader_name = shader_config["shader_set_name"]
+			delete shader_config[shader_name];
+			if (results[shader_name] === undefined) {
+				results[shader_name] = [shader_config];	
+			} else {
+				results[shader_name].push(shader_config);
+			}
+		}
+
+		return results;
+	}
+
 	return (
 		shader_configs.map((config_item, index) => {
 			switch (config_item.config_type_name) {
@@ -74,7 +108,7 @@ const ShaderConfigRows = () => {
 								<label htmlFor="config">{config_item.shader_config_name}:</label>
 							</div>
 							<div id="configElement" className="col-1">
-								<input id={config_item.shader_config_display_name} name={config_item.shader_config_name} type="field" defaultValue={searchParams.get(config_item.shader_config_name) ?? undefined}/>
+								<input id={config_item.shader_config_display_name} name={config_item.shader_config_name} type="field" defaultValue={search_params.get(config_item.shader_config_name) ?? undefined}/>
 							</div>
 						</div>
 					);
@@ -89,7 +123,7 @@ const ShaderConfigRows = () => {
 									key={ options_map[config_item.shader_config_id]?.join(',') }
 									id={ config_item.shader_config_name } 
 									name={ config_item.shader_config_name } 
-									defaultValue={ searchParams.get(config_item.shader_config_name) ?? undefined }
+									defaultValue={ search_params.get(config_item.shader_config_name) ?? undefined }
 								> 	
 								{ options_map[config_item.shader_config_id]?.map((config_option, index) => {
 									return (
