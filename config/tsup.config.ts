770a219bf8deeac9c22b79394d1c015f9ef180a3
stampatron <jwstamp@gmail.com>
2026-05-19 03:37:53 +0000

    Updated the tsup config to include the debug_config.mts file as a seperate file via a unique entry point so that all code can continue to reference it using a relative file path.

--- config/tsup.config.ts
+++ config/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["server.mts", "api"],
+  entry: ["server.mts", "api/*", "src/libs/debug_config.mts"],
   format: ["esm"],
   platform: "node",
   bundle: false,
